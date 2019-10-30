4a236356c49975a1776986f86939d67ec6e0b674
jokerpheonix <56969300+jokerpheonix@users.noreply.github.com>
2019-10-30 13:42:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+//index
 <?php
 //include 'php_scripts/register.php';
     $dbname = 'hello';
@@ -36,35 +37,72 @@
     </div>
 </body>
 </html>
-<?php
-/*
-$user_check_query = "SELECT * FROM customers WHERE username='$username' OR email_id='$email' LIMIT 1";
-			$result = mysqli_query($conn, $user_check_query);
-			$user = mysqli_fetch_assoc($result);
-			if($user){
-				if($user['username'] === $username){
-					print '<script type="text/javascript">';
-					print 'alert("Username Exists");';
-					print '</script>';
-				}
-				else if($user['email'] === $email) {
-					print '<script type="text/javascript">';
-					print 'alert("Email ID Exists");';
-					print '</script>';
-				}
-            }
-            
 
-$user_check_query = "SELECT * FROM customers WHERE username='$user_login' AND password='$pwdencrypt' LIMIT 1";
-				$result = mysqli_query($conn, $user_check_query);
-				if(mysqli_num_rows($result) == 1){
-					$_SESSION['username'] = $user_login;
-					$_SESSION['success'] = "You are now logged in";
-					header('location: index.php');
-				}else {
-					print '<script type="text/javascript">';
-					print 'alert("Invalid username [or] password");';
-					print '</script>';
-				}
-                */
-                ?>
+//display
+
+<?php
+//include 'php_scripts/register.php';
+    $dbname = 'hello';
+    $servername = 'localhost';
+    $username = 'root';
+    $pwd = '';
+    $conn = new mysqli($servername,$username,$pwd,$dbname) or die(mysqli_error($conn));
+?>
+<html>
+<head></head>
+<body>
+<div style="align: center;">
+        <table>
+            <tr><th>id</th><th>username</th><th>mobile</th></tr>
+            <?php
+                $result = mysqli_query($conn,"SELECT * FROM `haii`");
+                while($row = mysqli_fetch_assoc($result)){
+            ?>
+            <tr><td><?php echo "".$row['name']; ?></td><td><?php echo "".$row['password']; ?></td></tr>
+            <?php
+                }
+            ?>
+        </table>
+    </div><br>
+</body>
+</html>
+
+//login
+
+<?php
+    $dbname = 'hello';
+    $servername = 'localhost';
+    $username = 'root';
+    $pwd = '';
+    $conn = new mysqli($servername,$username,$pwd,$dbname) or die(mysqli_error($conn));
+    if(isset($_POST['login'])){
+    	$name = mysqli_real_escape_string($conn,$_POST['username']);
+    	$pwd = mysqli_real_escape_string($conn,$_POST['password']);
+    	$user_check_query = "SELECT * FROM haii WHERE name='$name' AND password='$pwd'";
+					$result = mysqli_query($conn, $user_check_query);
+					if(mysqli_num_rows($result)){
+						$_SESSION['username'] = $user_login;
+						$_SESSION['success'] = "You are now logged in";
+						header('location: index.php');
+					}else {
+						print '<script type="text/javascript">';
+						print 'alert("Invalid username [or] password");';
+						print '</script>';
+					}    
+    }
+	?>
+<html>
+<title></title>
+<head></head>
+<body>
+    
+    <div style="text-align: center;">
+        <form method="POST" action="<?php 
+                 echo $_SERVER['PHP_SELF']; ?>">
+            <input type="text" name="username" placeholder="username">
+            <input type="text" name="password" placeholder="password"><br>
+            <button type="submit" name="login">login</button>
+        </form>
+    </div>
+</body>
+</html>
